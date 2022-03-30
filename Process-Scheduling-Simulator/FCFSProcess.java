eadf8e13baffd8f0d2e97a89b5cf29c43f0fe35f
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2022-03-31 00:02:40 +0900

    Add files via upload

---
+++ Process-Scheduling-Simulator/FCFSProcess.java
@@ -0,0 +1,12 @@
+
+public class FCFSProcess {
+	int arrivalTime;
+	int burstTime;
+	String name;
+	
+	public FCFSProcess(int arrivalTime, int BurstTIme, String name) {
+		this.arrivalTime = arrivalTime;
+		this.burstTime = BurstTIme;
+		this.name = name;
+	}
+}
